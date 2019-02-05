5eb00b8f307b480048a0be697f8f6ee506fa9faa
Thomas Hehl <thom@heavyweightsoftware.com>
2019-02-05 17:38:08 -0500

    Finish data model v3

--- src/main/java/com/bugreserve/manage/model/project/ProjectMember.java
+++ src/main/java/com/bugreserve/manage/model/project/ProjectMember.java
@@ -1,6 +1,13 @@
 package com.bugreserve.manage.model.project;
 
+import com.bugreserve.manage.model.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.annotation.CreatedDate;
+
 import javax.persistence.*;
+import java.util.Calendar;
 
 @Table
 @Entity(name="project_member")
@@ -10,6 +17,24 @@ public class ProjectMember {
     @Column(nullable = false)
     private Long id;
 
-    @Column(name="user_name", nullable = false)
-    private String userName;
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User user;
+
+    @JoinColumn(name = "project_id", nullable = false)
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Project project;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="start_date", nullable = false)
+    @CreatedDate
+    private Calendar start;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="end_date", nullable = false)
+    private Calendar end;
 }
